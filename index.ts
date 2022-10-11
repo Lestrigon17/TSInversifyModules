0ffb9c1b90c9dd7a993330710bba5e6e5e56a524
Lestrigon17 <lestrigon17@yandex.ru>
2022-10-11 13:46:27 +0500

    Update

--- index.ts
+++ index.ts
@@ -3,5 +3,12 @@ import "./Modules/Modules.Imports";
 
 import {ModuleSystem} from "./Modules/Modules.Entry";
 
-const testCase = ModuleSystem.service.Get(ModuleSystem.Alias.TestCase);
-testCase.Run();
+const testCase = ModuleSystem.service.Get(ModuleSystem.Alias.ExampleModule);
+let a = new testCase.namespace.TestItem("iasdia", testCase.namespace.EItemType.PRIVATE);
+
+
+// InstanceType
+let b!: InstanceType<typeof testCase.namespace.TestItem>
+
+// Static type
+let c: typeof testCase.namespace.TestItem;
